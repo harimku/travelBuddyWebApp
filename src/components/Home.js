e60ca124df0adf6ba1c918a393cdbafe7bab1c18
harimku <hrku19@gmail.com>
2023-04-14 18:00:20 -0700

    Install UI library & set up home page view

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+
+
+export default function Home() {
+  return (
+    <div className="content">
+      <p>Home page content</p>
+    </div >
+  );
+}
