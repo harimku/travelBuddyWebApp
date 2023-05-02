895d01de2be359ffeadda82a739d825e3f34b71c
harrison-CSC <hrondeau@mail.sfsu.edu>
2023-05-02 14:39:22 -0700

    implemented review page & homepage button

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header.js';
 import Home from './components/Home/Home.js';
+import Reviews from './components/Reviews/Reviews.js';
 
 
 function App() {
   const [searchResults, setSearchResults] = useState({});
-  const [username, setUsername] = useState();
 
   return (
     <div className="App">
       <Router>
         {/* display Header component in every page */}
-        <Header setSearchResults={setSearchResults} setUsername={setUsername} />
+        <Header setSearchResults={setSearchResults} />
 
         {/* display content based on URL route */}
         <Routes>
           <Route path="/" element={<Home searchResults={searchResults} />} />
+          <Route path="/writeReview" element={<Reviews />} />
+
         </Routes>
       </Router>
     </div>
