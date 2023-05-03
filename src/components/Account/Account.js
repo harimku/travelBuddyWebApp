211c195486aed7f20df4cdf43c8fe1ef026fab20
Donghyun Nam <dnam@mail.sfsu.edu>
2023-05-03 16:05:27 -0700

    Initialized Account page and subpages

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,10 +1,65 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import '../../App.css';
+import { Col, Row, Menu } from 'antd';
+import {
+    HeartFilled,
+    EditFilled,
+    SettingOutlined,
+    LogoutOutlined
+} from '@ant-design/icons';
+
+// Menu tab items
+const items = [
+    getItem('Saved List', '1', <HeartFilled />),
+    getItem('My Reviews', '2', <EditFilled />),
+    getItem('Setting', '3', <SettingOutlined />),
+    getItem(),
+    getItem('Logout', '5', <LogoutOutlined />),
+];
+
+function getItem(label, key, icon, children) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
+}
 
 export default function Account() {
-  return (
-    <div>
-      Account Page
-    </div>
-  );
+    const [chosenTab, setChosenTab] = useState('1');
+
+    const handleClick = (op) => {
+        console.log(op);
+        setChosenTab(op.key);
+    };
+
+    return (
+        <div>
+            <br /><br />
+            <Row>
+                <Col
+                    xs={{ span: 8 }}
+                    sm={{ span: 6 }}
+                    md={{ span: 8 }}
+                    lg={{ span: 8 }}
+                >
+                    <Menu
+                        onClick={handleClick}
+                        mode="vertical"
+                        defaultSelectedKeys={[chosenTab]}
+                        items={items}
+                    />
+                </Col>
+                <Col
+                    xs={{ span: 16 }}
+                    sm={{ span: 18 }}
+                    md={{ span: 16 }}
+                    lg={{ span: 16 }}
+                >
+                    Display content here
+                </Col>
+            </Row>
+        </div>
+    );
 };
