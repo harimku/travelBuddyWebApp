fc3854330fe12db59b4d82049556a51d6e79b75f
wilsonyinn <68367621+wilsonyinn@users.noreply.github.com>
2023-05-03 16:58:55 -0700

    Added contents to Item Details page

--- src/components/Card/ItemDetail.js
+++ src/components/Card/ItemDetail.js
@@ -1,11 +1,36 @@
 import React from "react";
+import { Button, Card, Col, Image, Row } from "antd";
 
 export default function ItemDetail({ data }) {
   return (
     <div>
-      Item detail:
-      <p>title: {data.title}</p>
-      <p>location: {data.location}</p>
+      <Card>
+        <Row>
+          <Col
+            xs={{ span: 24 }}
+            sm={{ span: 12 }}
+            md={{ span: 10 }}
+            lg={{ span: 10 }}
+          >
+            <Image src={data.image} />
+          </Col>
+          <Col
+            xs={{ span: 24 }}
+            sm={{ span: 12 }}
+            md={{ span: 14 }}
+            lg={{ span: 14 }}
+          >
+            <h2>{data.title}</h2>
+            <p>Location: {data.location}</p>
+            <br />
+            <br />
+            <Button type="primary">Leave a Review</Button>
+            <Button type="primary" style={{ margin: "1em" }}>
+              Save to My List
+            </Button>
+          </Col>
+        </Row>
+      </Card>
     </div>
   );
 }
