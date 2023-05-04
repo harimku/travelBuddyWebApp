79e19c4f8c99e77c93b71b05576492b8c91e473b
harrison-CSC <hrondeau@mail.sfsu.edu>
2023-05-04 14:14:39 -0700

    Added account tab contents

--- src/components/Account/MyReviews.js
+++ src/components/Account/MyReviews.js
@@ -1,8 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import '../../App.css';
+import { Card, Row, Menu } from "antd";
+import { EditFilled } from "@ant-design/icons";
+
+const items = [
+  {
+    label: 'Visit',
+    key: '1',
+  },
+  {
+    label: 'Eat',
+    key: '2',
+  },
+  {
+    label: 'Activities',
+    key: '3',
+  },
+  {
+    label: 'Hotels',
+    key: '4',
+  },
+]
 
 export default function MyReviews() {
+  const [content, setContent] = useState();
+  const [chosenTab, setChosenTab] = useState('1');
+
+  const handleTabChange = (chosen) => {
+    console.log(chosen);
+    switch (chosen) {
+      case '1':
+        setChosenTab("visit");
+        break;
+      case '2':
+        setChosenTab("eat");
+        break;
+      case '3':
+        setChosenTab("activities");
+        break;
+      case '4':
+        setChosenTab("hotels");
+        break;
+      default:
+        break;
+    }
+    setContent(chosen.label);
+  };
+
   return (
-    <p>My Reviews</p>
+    <Card bordered={false}>
+      <Row align="middle">
+        <h2><EditFilled />    My Reviews</h2>
+      </Row>
+      <Menu
+        onClick={handleTabChange}
+        selectedKey={[chosenTab]}
+        mode="horizontal"
+        items={items} />
+      <Card>
+        {content}
+      </Card>
+    </Card>
   );
 };
--- src/components/Account/SavedList.js
+++ src/components/Account/SavedList.js
@@ -1,8 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import '../../App.css';
+import { Card, Row, Menu } from "antd";
+import { HeartFilled } from "@ant-design/icons";
+
+const items = [
+  {
+    label: 'Visit',
+    key: '1',
+  },
+  {
+    label: 'Eat',
+    key: '2',
+  },
+  {
+    label: 'Activities',
+    key: '3',
+  },
+  {
+    label: 'Hotels',
+    key: '4',
+  },
+]
 
 export default function SavedList() {
+
+  const [content, setContent] = useState();
+  const [chosenTab, setChosenTab] = useState('1');
+
+  const handleTabChange = (chosen) => {
+    console.log(chosen);
+    switch (chosen) {
+      case '1':
+        setChosenTab("visit");
+        break;
+      case '2':
+        setChosenTab("eat");
+        break;
+      case '3':
+        setChosenTab("activities");
+        break;
+      case '4':
+        setChosenTab("hotels");
+        break;
+      default:
+        break;
+    }
+    setContent(chosen.label);
+  };
+
   return (
-    <p>Saved List</p>
+    <div>
+      <Card bordered={false}>
+        <Row align="middle">
+          <h2><HeartFilled />    Saved List</h2>
+        </Row>
+        <Menu
+          onClick={handleTabChange}
+          selectedKey={[chosenTab]}
+          mode="horizontal"
+          items={items} />
+
+        <Card>
+          {content}
+        </Card>
+      </Card>
+    </div >
   );
 };
